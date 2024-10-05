7aaa55426f23d23b16e13a7fc313ef9a68cad712
Danylo Levytskyi <alfa7.plast@gmail.com>
2024-10-05 12:27:00 +0300

    edit migrations

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 python manage.py makemigrations user albums
+pyththon manage.py makemigrations
+python manage.py migrate user albums
 python manage.py migrate
-gunicorn MyBeautifulAlbums.wsgi:application 
+gunicorn MyBeautifulAlbums.wsgi:application
